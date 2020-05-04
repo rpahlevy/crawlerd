841e5b5c5c796be931cde5e2077adbf9850c073d
rpahlevy <43176811+rpahlevy@users.noreply.github.com>
2020-05-04 10:52:49 +0700

    init

---
+++ crawlerd/spiders/gamelab.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class GamelabSpider(scrapy.Spider):
+    name = 'gamelab'
+    allowed_domains = ['gamelab.id']
+    start_urls = ['http://gamelab.id/']
+
+    def parse(self, response):
+        url = response.url
+
+        # get domain
+        domain = url.split('/')[2]#.replace('www.', '')
+
+        body = response.xpath('normalize-space(//body)').extract_first()
+
+        for href in response.css('a::attr(href)'):
+            href_domain = href.split('/')[2]
+            href_domain_nw = href_domain.replace('www.', '')
+
+            if href_domain in self.allowed_domains or href_domain_nw in self.allowed_domains:
+                yield response.follow(href, self.parse)
