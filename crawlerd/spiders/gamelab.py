e0a861d21465748f575d431895abf33692df8a99
rpahlevy <43176811+rpahlevy@users.noreply.github.com>
2020-05-04 11:08:05 +0700

    fix error href

--- crawlerd/spiders/gamelab.py
+++ crawlerd/spiders/gamelab.py
@@ -17,8 +17,10 @@ class GamelabSpider(scrapy.Spider):
 
         for href in response.css('a::attr(href)'):
             href = href.get()
+            if 'http' not in href:
+                yield response.follow(href, self.parse)
+
             href_domain = href.split('/')[2]
             href_domain_nw = href_domain.replace('www.', '')
-
-            if 'http' not in href or href_domain in self.allowed_domains or href_domain_nw in self.allowed_domains:
+            if href_domain in self.allowed_domains or href_domain_nw in self.allowed_domains:
                 yield response.follow(href, self.parse)
